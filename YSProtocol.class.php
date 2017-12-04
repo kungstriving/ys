371481e93a7b274ae9514778ba3c296c75bd46b1
kungstriving <kungstriving@qq.com>
2017-12-04 22:47:39 +0800

    添加搜索设备、列表设备功能

--- YSProtocol.class.php
+++ YSProtocol.class.php
@@ -10,6 +10,8 @@ class YSProtocol {
     const LOGOUT_GATE_CMDCODE = 3;
     const HEARTBEAT_GATE_CMDCODE = 4;
     const SERVER_IDENTIFY_GATE_CMDCODE = 5;
+    const SEARCH_NEW_DEVS_GATE_CMDCODE = 10;
+    const LIST_ALL_DEVS_GATE_CMDCODE = 11;
     
     /**
      * 根据json消息返回二进制数据
@@ -69,7 +71,7 @@ class YSProtocol {
         echo "[YSProtocol::decodeMsg] for -- ".bin2hex($msgBin)."\n\n";
         
         $msgJson;
-        
+        $msgBinReal = hex2bin($msgBin);
         $commonFormat = "@4/".
                         "n1msgLen/".
                         "n1sign/".
@@ -80,21 +82,28 @@ class YSProtocol {
                         "n1objID"
                         ;
         
-        $commonArray = unpack($commonFormat, $msgBin);
+        $commonArray = unpack($commonFormat, $msgBinReal);
         $objType = $commonArray["objType"];
+        
         
         $dataArray;
         $msgCRC;
         switch ($objType) {
             case YSProtocol::GATE_OBJ_TYPE:
-                $dataArray = YSProtocol::decodeGateMsg($msgBin, $msgCRC);
+                
+                $dataArray = YSProtocol::decodeGateMsg($msgBinReal, $msgCRC);
                 break;
         }
         
         //加入CRC
         $commonArray["crc"] = $msgCRC;
+        
         $msgArray = array_merge($commonArray, $dataArray);
+//         $msgArray[""]
+        var_dump($msgArray);
+        
         $msgJson = json_encode($msgArray);
+        
         echo "[YSProtocol::decodeMsg] result in json is " . $msgJson . "\n\n";
         return $msgJson;
     }
@@ -117,22 +126,23 @@ class YSProtocol {
                     //正确
                     $cmdFormat = "@16/".
                         "n1cmdRetCode/".
-                        "a32username/".
-                        "a16phoneNum/".
-                        "a8gateID/".
+                        "A32username/".
+                        "A16phoneNum/".
+                        "A8gateID/".
                         "n1sign/".
                         "n1gateFixLen/".
                         "n1gateExtLen/".
                         "n1protoVer/".
-                        "a8gateID2/".
-                        "a6gateMAC/".
+                        "A8gateID2/".
+                        "A6gateMAC/".
                         "@128/".
-                        "a16gateName/".
+                        "A16gateName/".
                         "n1hbLan/".
                         "n1hbWLan/".
-                        "a68netData/".
+                        "@216/".
                         "n1crc";
                     $cmdArr = unpack($cmdFormat, $msgBin);
+                    
                     $msgCRC = $cmdArr["crc"];
                 } else {
                     //错误
@@ -195,6 +205,78 @@ class YSProtocol {
                 $cmdArr = unpack($cmdFormat, $msgBin);
                 $msgCRC = $cmdArr["crc"];
                 
+                break;
+            case YSProtocol::SERVER_IDENTIFY_GATE_CMDCODE:
+                //服务器识别网关的回应
+                $cmdFormat = "@16/n1cmdRetCode";
+                $cmdArr = unpack($cmdFormat, $msgBin);
+                $cmdRetCode = $cmdArr["cmdRetCode"];
+                if ($cmdRetCode == 0) {
+                    //正确
+                    $cmdFormat = "@16/".
+                        "n1cmdRetCode/".
+                        "a16serverID/".
+                        "n1sign/".
+                        "n1gateFixLen/".
+                        "n1gateExtLen/".
+                        "n1protoVer/".
+                        "a8gateID/".
+                        "a6gateMAC/".
+                        "@88/".
+                        "a16gateName/".
+                        "n1hbLan/".
+                        "n1hbWLan/".
+                        "a68reserved/".
+                        "n1crc";
+                    $cmdArr = unpack($cmdFormat, $msgBin);
+                    $msgCRC = $cmdArr["crc"];
+                } else {
+                    //错误
+                    $cmdFormat = "@16/".
+                        "n1cmdRetCode/".
+                        "a8serverMAC/".
+                        "n1crc";
+                    $cmdArr = unpack($cmdFormat, $msgBin);
+                    $msgCRC = $cmdArr["crc"];
+                }
+                break;
+            case YSProtocol::SEARCH_NEW_DEVS_GATE_CMDCODE:
+                //服务器搜索新设备的回应
+                $cmdFormat="@16/n1cmdRetCode/n1crc";
+                $cmdArr = unpack($cmdFormat, $msgBin);
+                $msgCRC = $cmdArr["crc"];
+                break;
+            case YSProtocol::LIST_ALL_DEVS_GATE_CMDCODE:
+                //服务器列表所有终端的回应
+                $cmdFormat = "@16/n1cmdRetCode";
+                $cmdArr = unpack($cmdFormat, $msgBin);
+                $cmdRetCode = $cmdArr["cmdRetCode"];
+                
+                if ($cmdRetCode == 0) {
+                    //正确，读取站点数量
+                    $cmdFormat = "@18/n1devNum";
+                    $cmdArr = unpack($cmdFormat, $msgBin);
+                    $devNum = $cmdArr["devNum"];
+                    echo "[debug ---]devnum".$devNum."\n";
+                    $cmdFormat = "@20/";
+                    
+                    for ($i = 0; $i < $devNum; $i++) {
+                        $cmdFormat = $cmdFormat."n1dev".$i."ID/n1dev".$i."Type/H8dev".$i."MAC/";
+                    }
+                    
+                    $cmdFormat = $cmdFormat."n1crc";
+                    
+                    $cmdArr = unpack($cmdFormat, $msgBin);
+                    $cmdArr["devNum"] = $devNum;
+                    $msgCRC = $cmdArr["crc"];
+                } else {
+                    //错误
+                    $cmdFormat = "@16/".
+                        "n1cmdRetCode/".
+                        "n1crc";
+                    $cmdArr = unpack($cmdFormat, $msgBin);
+                    $msgCRC = $cmdArr["crc"];
+                }
                 break;
         }
         
@@ -294,6 +376,30 @@ class YSProtocol {
                 $packBin = $propRegionBin.$objTypeBin.$cmdCodeBin
                     .$objIDBin.$reservedBin.$dataBin;
                 $msgLen = 18+18;
+                break;
+            case YSProtocol::SEARCH_NEW_DEVS_GATE_CMDCODE:
+                //0A开始
+                echo "\n--- search devs ---\n";
+                $propRegion = 0x8000;
+                $propRegionBin = pack("n", $propRegion);
+                $objTypeBin = pack("C", $objType);
+                $cmdCodeBin = pack("C", $cmdCode);
+                $objIDBin = pack("n", $objID);
+                $lastSecs = pack("n", $msgJsonObj->data->lastSecs);
+                $packBin = $propRegionBin.$objTypeBin.$cmdCodeBin
+                    .$objIDBin.$lastSecs;
+                $msgLen = 18+2;
+                break;
+            case YSProtocol::LIST_ALL_DEVS_GATE_CMDCODE:
+                echo "\n --- list all devices ---\n";
+                $propRegion = 0x8000;
+                $propRegionBin = pack("n", $propRegion);
+                $objTypeBin = pack("C", $objType);
+                $cmdCodeBin = pack("C", $cmdCode);
+                $objIDBin = pack("n", $objID);
+                $packBin = $propRegionBin.$objTypeBin.$cmdCodeBin
+                    .$objIDBin;
+                $msgLen = 18+0;
                 break;
         }
         
