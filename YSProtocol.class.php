d836163845c04008b5dd4eb30a00ded82f1a32a1
kungstriving <kungstriving@qq.com>
2018-01-09 22:49:00 +0800

    Update YSProtocol.class.php

--- YSProtocol.class.php
+++ YSProtocol.class.php
@@ -9,8 +9,15 @@ include 'YSProtocol.Clock.class.php';
 include 'YSProtocol.Location.class.php';
 include 'YSProtocol.TaskTable.class.php';
 include 'YSProtocol.Helper.class.php';
+
 // include 'YSProtocol.Gate.class.min.php';
 // include 'YSProtocol.Device.class.min.php';
+// include 'YSProtocol.Group.class.min.php';
+// include 'YSProtocol.StaticScene.class.min.php';
+// include 'YSProtocol.DynScene.class.min.php';
+// include 'YSProtocol.Clock.class.min.php';
+// include 'YSProtocol.Location.class.min.php';
+// include 'YSProtocol.TaskTable.class.min.php';
 // include 'YSProtocol.Helper.class.min.php';
 
 class Third_Ys_Sdk {
@@ -31,7 +38,7 @@ class Third_Ys_Sdk {
     /**
      * 根据json消息返回二进制数据
      * 
-     * @param unknown $msgJson
+     * @param $msgJson
      */
     public static function encodeMsg($msgJson) {
         echo "[YSProtocol::encodeMsg] for -- " . $msgJson. "\n\n";
@@ -53,38 +60,38 @@ class Third_Ys_Sdk {
         $dataBin;
         switch ($objType) {
             case Third_Ys_Sdk::GATE_OBJ_TYPE:
-                $dataBin = GateYSProtocol::encodeGateMsg($msgJsonObj, $msgLen);
+                $dataBin = Third_Ys_Gatesdk::encodeGateMsg($msgJsonObj, $msgLen);
                 break;
             case Third_Ys_Sdk::DEVICE_TYPE:
-                $dataBin = DeviceYSProtocol::encodeDeviceMsg($msgJsonObj, $msgLen);
+                $dataBin = Third_Ys_Devicesdk::encodeDeviceMsg($msgJsonObj, $msgLen);
                 break;
             case Third_Ys_Sdk::GROUP_TYPE:
                 //分组
-                $dataBin = GroupYSProtocol::encodeGroupMsg($msgJsonObj, $msgLen);
+                $dataBin = Third_Ys_Groupsdk::encodeGroupMsg($msgJsonObj, $msgLen);
                 break;
             case Third_Ys_Sdk::STATIC_SCENE_TYPE:
                 //静态场景
-                $dataBin = StaticSceneYSProtocol::encodeStaticSceneMsg($msgJsonObj, $msgLen);
+                $dataBin = Third_Ys_Staticscenesdk::encodeStaticSceneMsg($msgJsonObj, $msgLen);
                 break;
             case Third_Ys_Sdk::DYNAMIC_SCENE_TYPE:
                 //动态场景
-                $dataBin = DynSceneYSProtocol::encodeDynSceneMsg($msgJsonObj, $msgLen);
+                $dataBin = Third_Ys_Dynscenesdk::encodeDynSceneMsg($msgJsonObj, $msgLen);
                 break;
             case Third_Ys_Sdk::CLOCK_TYPE:
                 //闹钟
-                $dataBin = ClockYSProtocol::encodeClockMsg($msgJsonObj, $msgLen);
+                $dataBin = Third_Ys_Clocksdk::encodeClockMsg($msgJsonObj, $msgLen);
                 break;
             case Third_Ys_Sdk::LOCATION_TYPE:
                 //地址围栏
-                $dataBin = LocationYSProtocol::encodeLocationMsg($msgJsonObj, $msgLen);
+                $dataBin = Third_Ys_Locationsdk::encodeLocationMsg($msgJsonObj, $msgLen);
                 break;
             case Third_Ys_Sdk::TASKTABLE_TYPE:
                 //任务表
-                $dataBin = TaskTableYSProtocol::encodeTaskMsg($msgJsonObj, $msgLen);
+                $dataBin = Third_Ys_Tasktablesdk::encodeTaskMsg($msgJsonObj, $msgLen);
                 break;
             case Third_Ys_Sdk::ALL_TYPE:
                 //所有类型
-                $dataBin = DeviceYSProtocol::encodeDeviceMsg($msgJsonObj, $msgLen);
+                $dataBin = Third_Ys_Devicesdk::encodeDeviceMsg($msgJsonObj, $msgLen);
                 break;
             default:
                 
@@ -137,31 +144,31 @@ class Third_Ys_Sdk {
         switch ($objType) {
             case Third_Ys_Sdk::GATE_OBJ_TYPE:
                 
-                $dataArray = GateYSProtocol::decodeGateMsg($msgBinReal, $msgCRC);
+                $dataArray = Third_Ys_Gatesdk::decodeGateMsg($msgBinReal, $msgCRC);
                 break;
             case Third_Ys_Sdk::DEVICE_TYPE:
-                $dataArray = DeviceYSProtocol::decodeDeviceMsg($msgBinReal, $msgCRC);
+                $dataArray = Third_Ys_Devicesdk::decodeDeviceMsg($msgBinReal, $msgCRC);
                 break;
             case Third_Ys_Sdk::GROUP_TYPE:
-                $dataArray = GroupYSProtocol::decodeGroupMsg($msgBinReal, $msgCRC);
+                $dataArray = Third_Ys_Groupsdk::decodeGroupMsg($msgBinReal, $msgCRC);
                 break;
             case Third_Ys_Sdk::STATIC_SCENE_TYPE:
-                $dataArray = StaticSceneYSProtocol::decodeStaticSceneMsg($msgBinReal, $msgCRC);
+                $dataArray = Third_Ys_Staticscenesdk::decodeStaticSceneMsg($msgBinReal, $msgCRC);
                 break;
             case Third_Ys_Sdk::DYNAMIC_SCENE_TYPE:
-                $dataArray = DynSceneYSProtocol::decodeDynSceneMsg($msgBinReal, $msgCRC);
+                $dataArray = Third_Ys_Dynscenesdk::decodeDynSceneMsg($msgBinReal, $msgCRC);
                 break;
             case Third_Ys_Sdk::CLOCK_TYPE:
-                $dataArray = ClockYSProtocol::decodeClockMsg($msgBinReal, $msgCRC);
+                $dataArray = Third_Ys_Clocksdk::decodeClockMsg($msgBinReal, $msgCRC);
                 break;
             case Third_Ys_Sdk::LOCATION_TYPE:
-                $dataArray = LocationYSProtocol::decodeLocationMsg($msgBinReal, $msgCRC);
+                $dataArray = Third_Ys_Locationsdk::decodeLocationMsg($msgBinReal, $msgCRC);
                 break;
             case Third_Ys_Sdk::TASKTABLE_TYPE:
-                $dataArray = TaskTableYSProtocol::decodeTaskMsg($msgBinReal, $msgCRC);
+                $dataArray = Third_Ys_Tasktablesdk::decodeTaskMsg($msgBinReal, $msgCRC);
                 break;
             case Third_Ys_Sdk::ALL_TYPE:
-                $dataArray = DeviceYSProtocol::decodeDeviceMsg($msgBinReal, $msgCRC);
+                $dataArray = Third_Ys_Devicesdk::decodeDeviceMsg($msgBinReal, $msgCRC);
                 break;
         }
         
