dd195ddf2c02f598e2895c1dcf438387ee9789b6
kungstriving <kungstriving@qq.com>
2018-01-21 17:09:59 +0800

    更新闹钟

--- test.php
+++ test.php
@@ -1,42 +1,70 @@
 <?php
+include 'YSProtocol.Helper.class.php';
+// $testConfigReadAllDev = '{
+//     "sign":50210,
+//     "msgID":108,
+//     "objType":4,
+//     "cmdCode":192,
+//     "objID":65535,
+//     "crc":255
+// }';
 
-$testConfigReadAllDev = '{
-    "sign":50210,
-    "msgID":108,
-    "objType":4,
-    "cmdCode":192,
-    "objID":65535,
-    "crc":255
-}';
-
-$msgJsonObj = json_decode($testConfigReadAllDev);
+// $msgJsonObj = json_decode($testConfigReadAllDev);
 
 
-if (property_exists($msgJsonObj, "sliceM")) {
-    echo "OK\n";
-} else {
-    echo "hahah\n";
-}
+// if (property_exists($msgJsonObj, "sliceM")) {
+//     echo "OK\n";
+// } else {
+//     echo "hahah\n";
+// }
 
-// echo pack('H*',"621176845bb6");
+// // echo pack('H*',"621176845bb6");
 
-// echo chr(hexdec("621176845bb6"));
+// // echo chr(hexdec("621176845bb6"));
 
-function unicode_decode($name){
+// function unicode_decode($name){
     
-    $json = '{"str":"'.$name.'"}';
-    $arr = json_decode($json,true);
-    if(empty($arr)) return '';
-    return $arr['str'];
-}
+//     $json = '{"str":"'.$name.'"}';
+//     $arr = json_decode($json,true);
+//     if(empty($arr)) return '';
+//     return $arr['str'];
+// }
 
-$testStr = "621176845bb600000000000000000000";
-$testStr = chunk_split($testStr,4,"\u");
-$testStr = "\u".$testStr;
-$testStr = substr($testStr, 0, (strlen($testStr) - 2));
+// $testStr = "621176845bb600000000000000000000";
+// $testStr = chunk_split($testStr,4,"\u");
+// $testStr = "\u".$testStr;
+// $testStr = substr($testStr, 0, (strlen($testStr) - 2));
 
-echo $testStr . "\n";
-$aihao = unicode_decode($testStr);
-echo $aihao;
+// echo $testStr . "\n";
+// $aihao = unicode_decode($testStr);
+// echo $aihao;
 
+$teststr = "孔晓阳";
+// echo Third_Ys_Helpersdk::utf8_str_to_unicode($teststr);
+echo Third_Ys_Helpersdk::unicode_encode($teststr);
+
+echo "\n";
+
+$unistr = "5b5466539633";
+//"3664346238626435"
+echo Third_Ys_Helpersdk::decodeUnicodeStr($unistr);
+
+echo "\n";
+
+$objType = 4;
+$objTypeBin = pack("C", $objType);
+
+$phone = "13366666666";
+$phone = Third_Ys_Helpersdk::ascEncode($phone);
+$phoneBin = pack("a16", $phone);
+
+echo $phoneBin;
+
+$testHex = "00d0f60020bc1355";
+echo "\n".pack("H16", $testHex)."\n";
+echo "\n".bin2hex(pack("H16", $testHex))."\n";
+
+$arrtemp = unpack("H32", $testHex);
+
+var_dump($arrtemp);
 
